d45b3287b1c597dc638adeb0033b68e72d1e7dc3
Heye Voecking <heye.voecking@gmail.com>
2018-12-14 15:01:16 +0100

    feat(frontend): add splash page

---
+++ frontend/src/app/pages/splash/splash.module.ts
@@ -0,0 +1,22 @@
+import { SplashPage } from './splash.page';
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+import { IonicModule } from '@ionic/angular';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    FormsModule,
+    IonicModule,
+    RouterModule.forChild([
+      {
+        path: '',
+        component: SplashPage,
+      },
+    ]),
+  ],
+  declarations: [SplashPage],
+})
+export class SplashPageModule {}
